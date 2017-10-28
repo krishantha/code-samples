398b0c1891583bac01ce6a09a003f70d9dea788a
Krishantha Dinesh <krishantha@krishantha.com>
2017-10-28 20:13:11 +0530

    Add sample microservice project

---
+++ sample-microservice/src/main/java/com/krishantha/sample/microservice/model/Response.java
@@ -0,0 +1,13 @@
+package com.krishantha.sample.microservice.model;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+import lombok.Data;
+
+public @Data
+class Response {
+
+
+   private   String code;
+    private  String message;
+}
